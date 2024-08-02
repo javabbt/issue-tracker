c2e3d1eb1ae52389e0ee2b22603a46d93c1af54d
Yannick Nyami <yannick.nyami@adevinta.com>
2024-08-02 18:01:22 +0200

    style the active links in the nav bar

---
+++ app/Navbar.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Link from "next/link";
+import React from "react";
+import { FaBug } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/"><FaBug/></Link>
+      <ul className="flex space-x-4">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link className={
+              classnames(
+                {'text-zinc-900' : link.href === currentPath,
+                'text-zinc-500' : link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors' : true}
+              )
+            } href={link.href}>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
