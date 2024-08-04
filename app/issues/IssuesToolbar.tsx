aa4fbe5d6860ebe3241b0da68e97dca64e695aa8
Yannick Nyami <yannick.nyami@adevinta.com>
2024-08-04 17:20:21 +0200

    fix link import

--- app/issues/IssuesToolbar.tsx
+++ app/issues/IssuesToolbar.tsx
@@ -1,11 +1,14 @@
-import { Button, Link } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuesToolbar = () => {
   return (
     <div className="mb-5">
       <Button>
-        <Link href="issues/new">New issue</Link>
+        <Link className="red" href="issues/new">
+          New issue
+        </Link>
       </Button>
     </div>
   );
