d89f6c053df30ec178ce9a8356a8673a9cd7aeec
Yannick Nyami <yannick.nyami@adevinta.com>
2024-08-05 13:22:53 +0200

    build api for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,39 @@
+import { issueSchema } from "@/app/schemaValidations";
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json("Issue not found", { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: parseInt(params.id) },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
-import { createIssueSchema } from "@/app/schemaValidations";
+import { issueSchema } from "@/app/schemaValidations";
 
 export async function POST(req: NextRequest) {
   const res = await req.json();
-  const isSafe = createIssueSchema.safeParse(res);
+  const isSafe = issueSchema.safeParse(res);
   if (!isSafe.success) {
     return NextResponse.json({ error: isSafe.error.errors }, { status: 400 });
   }
