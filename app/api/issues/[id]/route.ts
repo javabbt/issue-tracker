b121102007c20b7d7b0b9c9c90088ea4e4c50944
Yannick Nyami <yannick.nyami@adevinta.com>
2024-08-06 13:40:19 +0200

    Enhance the api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/schemaValidations";
+import { patchIssueSchema } from "@/app/schemaValidations";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -17,12 +17,20 @@ export async function PATCH(
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(
       { error: validation.error.errors },
       { status: 400 }
     );
+  }
+
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user) {
+      return NextResponse.json("User not found", { status: 404 });
   }
 
   const issue = await prisma.issue.findUnique({
@@ -38,6 +46,7 @@ export async function PATCH(
     data: {
       title: body.title,
       description: body.description,
+      assignedToUserId: body.assignedToUserId,
     },
   });
 
