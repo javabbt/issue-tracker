4afafc8ce9b1ed3e911c893a1d6fd1a32eeacc9a
Yannick Nyami <yannick.nyami@adevinta.com>
2024-08-07 20:42:36 +0200

    Build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,26 @@ import Image from "next/image";
 import "./theme-config.css";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import { prisma } from "@/prisma/client";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return <LatestIssues />;
+  const openedIssues = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closedIssues = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const inProgressIssues = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <IssueSummary
+      open={openedIssues}
+      closed={closedIssues}
+      inProgress={inProgressIssues}
+    />
+  );
 }
